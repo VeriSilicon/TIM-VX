096d99e0689b69777aeedf98689bb344efb70b39
Sven <xiang.zhang@verisilicon.com>
2021-05-13 22:28:36 +0800

    Support local sdk for cmake (#41)
    * Support local sdk for cmake
    
    cmake -DEXTERNAL_VIV_SDK=<Local_Driver_build_sdk_dir>
    
    Signed-off-by: xiang.zhang <xiang.zhang@verisilicon.com>

---
+++ cmake/local_sdk.cmake
@@ -0,0 +1,19 @@
+set(PKG_NAME "OVXDRV")
+
+message("include driver sdk from ${EXTERNAL_VIV_SDK}")
+set(OVXDRV_INCLUDE_DIRS)
+list(APPEND OVXDRV_INCLUDE_DIRS
+        ${EXTERNAL_VIV_SDK}include
+        ${EXTERNAL_VIV_SDK}include/CL)
+
+set(OVXDRV_LIBRARIES)
+list(APPEND OVXDRV_LIBRARIES
+    ${EXTERNAL_VIV_SDK}/drivers/libCLC.so
+    ${EXTERNAL_VIV_SDK}/drivers/libGAL.so
+    ${EXTERNAL_VIV_SDK}/drivers/libOpenVX.so
+    ${EXTERNAL_VIV_SDK}/drivers/libOpenVXU.so
+    ${EXTERNAL_VIV_SDK}/drivers/libVSC.so
+    ${EXTERNAL_VIV_SDK}/drivers/libArchModelSw.so
+    ${EXTERNAL_VIV_SDK}/drivers/libNNArchPerf.so)
+
+mark_as_advanced(${OVXDRV_INCLUDE_DIRS} ${OVXDRV_LIBRARIES})
