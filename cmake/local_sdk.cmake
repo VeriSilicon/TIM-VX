e830a20e9c998a68f606f5eabe4653c0c86602b3
Sven <xiang.zhang@verisilicon.com>
2021-05-13 22:23:45 +0800

    Update local_sdk.cmake

--- cmake/local_sdk.cmake
+++ cmake/local_sdk.cmake
@@ -1,19 +1,19 @@
 set(PKG_NAME "OVXDRV")
 
-message("include driver sdk from ${VIP_SDK_DIR}")
+message("include driver sdk from ${EXTERNAL_VIV_SDK}")
 set(OVXDRV_INCLUDE_DIRS)
 list(APPEND OVXDRV_INCLUDE_DIRS
-        ${VIP_SDK_DIR}include
-        ${VIP_SDK_DIR}include/CL)
+        ${EXTERNAL_VIV_SDK}include
+        ${EXTERNAL_VIV_SDK}include/CL)
 
 set(OVXDRV_LIBRARIES)
 list(APPEND OVXDRV_LIBRARIES
-    ${VIP_SDK_DIR}/drivers/libCLC.so
-    ${VIP_SDK_DIR}/drivers/libGAL.so
-    ${VIP_SDK_DIR}/drivers/libOpenVX.so
-    ${VIP_SDK_DIR}/drivers/libOpenVXU.so
-    ${VIP_SDK_DIR}/drivers/libVSC.so
-    ${VIP_SDK_DIR}/drivers/libArchModelSw.so
-    ${VIP_SDK_DIR}/drivers/libNNArchPerf.so)
+    ${EXTERNAL_VIV_SDK}/drivers/libCLC.so
+    ${EXTERNAL_VIV_SDK}/drivers/libGAL.so
+    ${EXTERNAL_VIV_SDK}/drivers/libOpenVX.so
+    ${EXTERNAL_VIV_SDK}/drivers/libOpenVXU.so
+    ${EXTERNAL_VIV_SDK}/drivers/libVSC.so
+    ${EXTERNAL_VIV_SDK}/drivers/libArchModelSw.so
+    ${EXTERNAL_VIV_SDK}/drivers/libNNArchPerf.so)
 
 mark_as_advanced(${OVXDRV_INCLUDE_DIRS} ${OVXDRV_LIBRARIES})
