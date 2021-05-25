2faa8a004d21b28f9283e4641493831a23e7a3ff
zhao.xia <zhao.xia@verisilicon.com>
2021-05-25 15:44:12 +0800

    Set pool2d rounding policy to internal
    Update pool2d rounding policy to internal.
    
    Signed-off-by: zhao.xia <zhao.xia@verisilicon.com>

--- src/tim/vx/ops/pool2d.cc
+++ src/tim/vx/ops/pool2d.cc
@@ -49,6 +49,12 @@ Pool2d::Pool2d(Graph* graph, PoolType type, PadType padding,
   this->impl()->node()->nn_param.pool.stride[0] = stride_[0];
   this->impl()->node()->nn_param.pool.stride[1] = stride_[1];
   this->impl()->node()->nn_param.pool.pad_type = TranslatePadType(padding_);
+  DownScaleSizeRounding down_scale_size_rounding = DownScaleSizeRounding::FLOOR;
+  if (RoundType::CEILING == round_type_) {
+    down_scale_size_rounding = DownScaleSizeRounding::CEILING;
+  }
+  this->SetRoundingPolicy(OverflowPolicy::SATURATE, RoundingPolicy::TO_ZERO,
+    down_scale_size_rounding);
 }
 
 }  // namespace ops
