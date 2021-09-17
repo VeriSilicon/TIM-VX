073a79f46308c56258b5fbe19ee4321615680cc7
lilei <lilei_mymail@foxmail.com>
2021-09-17 10:44:50 +0800

    add ops.h to contain all operation header file

---
+++ include/tim/vx/ops.h
@@ -0,0 +1,83 @@
+/****************************************************************************
+*
+*    Copyright (c) 2020 Vivante Corporation
+*
+*    Permission is hereby granted, free of charge, to any person obtaining a
+*    copy of this software and associated documentation files (the "Software"),
+*    to deal in the Software without restriction, including without limitation
+*    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+*    and/or sell copies of the Software, and to permit persons to whom the
+*    Software is furnished to do so, subject to the following conditions:
+*
+*    The above copyright notice and this permission notice shall be included in
+*    all copies or substantial portions of the Software.
+*
+*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+*    DEALINGS IN THE SOFTWARE.
+*
+*****************************************************************************/
+#ifndef TIM_VX_OPS_H_
+#define TIM_VX_OPS_H_
+
+#include "tim/vx/ops/activations.h"
+#include "tim/vx/ops/addn.h"
+#include "tim/vx/ops/arg.h"
+#include "tim/vx/ops/batch2space.h"
+#include "tim/vx/ops/batchnorm.h"
+#include "tim/vx/ops/clip.h"
+#include "tim/vx/ops/concat.h"
+#include "tim/vx/ops/conv1d.h"
+#include "tim/vx/ops/conv2d.h"
+#include "tim/vx/ops/deconv1d.h"
+#include "tim/vx/ops/deconv.h"
+#include "tim/vx/ops/depth2space.h"
+#include "tim/vx/ops/dropout.h"
+#include "tim/vx/ops/elementwise.h"
+#include "tim/vx/ops/fullyconnected.h"
+#include "tim/vx/ops/gather.h"
+#include "tim/vx/ops/gathernd.h"
+#include "tim/vx/ops/groupedconv2d.h"
+#include "tim/vx/ops/instancenormalization.h"
+#include "tim/vx/ops/l2normalization.h"
+#include "tim/vx/ops/layernormalization.h"
+#include "tim/vx/ops/localresponsenormalization.h"
+#include "tim/vx/ops/logical.h"
+#include "tim/vx/ops/logsoftmax.h"
+#include "tim/vx/ops/matmul.h"
+#include "tim/vx/ops/maxpoolwithargmax.h"
+#include "tim/vx/ops/maxunpool2d.h"
+#include "tim/vx/ops/moments.h"
+#include "tim/vx/ops/nbg.h"
+#include "tim/vx/ops/pad.h"
+#include "tim/vx/ops/pool2d.h"
+#include "tim/vx/ops/reduce.h"
+#include "tim/vx/ops/relational_operations.h"
+#include "tim/vx/ops/reorg.h"
+#include "tim/vx/ops/reshape.h"
+#include "tim/vx/ops/resize1d.h"
+#include "tim/vx/ops/resize.h"
+#include "tim/vx/ops/reverse.h"
+#include "tim/vx/ops/scatternd.h"
+#include "tim/vx/ops/select.h"
+#include "tim/vx/ops/shuffle_channel.h"
+#include "tim/vx/ops/simple_operations.h"
+#include "tim/vx/ops/slice.h"
+#include "tim/vx/ops/softmax.h"
+#include "tim/vx/ops/space2batch.h"
+#include "tim/vx/ops/space2depth.h"
+#include "tim/vx/ops/spatial_transformer.h"
+#include "tim/vx/ops/split.h"
+#include "tim/vx/ops/squeeze.h"
+#include "tim/vx/ops/stack.h"
+#include "tim/vx/ops/stridedslice.h"
+#include "tim/vx/ops/tile.h"
+#include "tim/vx/ops/transpose.h"
+#include "tim/vx/ops/unidirectional_sequence_lstm.h"
+#include "tim/vx/ops/unstack.h"
+
+#endif /* TIM_VX_OPS_H_ */
