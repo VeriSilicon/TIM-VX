953966b09dda929a553bc036e842d3f08d8201c1
Kee <xuke537@hotmail.com>
2021-11-04 10:03:57 +0800

    Add unit tests for Div OP

--- src/tim/vx/ops/elementwise_test.cc
+++ src/tim/vx/ops/elementwise_test.cc
@@ -130,3 +130,108 @@ TEST(FloorDiv, shape_5_1_broadcast_uint8) {
     EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
     EXPECT_EQ(golden, output);
 }
+
+TEST(Div, shape_1_fp32) {
+    auto ctx = tim::vx::Context::Create();
+    auto graph = ctx->CreateGraph();
+
+    tim::vx::ShapeType io_shape({1});
+    tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32,
+                            io_shape, tim::vx::TensorAttribute::INPUT);
+    tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32,
+                            io_shape, tim::vx::TensorAttribute::OUTPUT);
+
+    auto input_tensor_x = graph->CreateTensor(input_spec);
+    auto input_tensor_y = graph->CreateTensor(input_spec);
+    auto output_tensor = graph->CreateTensor(output_spec);
+
+    std::vector<float> in_data_x = { 1 };
+    std::vector<float> in_data_y = { 0 };
+    std::vector<float> golden = { std::numeric_limits<float>::infinity() };
+
+    EXPECT_TRUE(input_tensor_x->CopyDataToTensor(in_data_x.data(), in_data_x.size()*4));
+    EXPECT_TRUE(input_tensor_y->CopyDataToTensor(in_data_y.data(), in_data_y.size()*4));
+    auto op = graph->CreateOperation<tim::vx::ops::Div>();
+    (*op).BindInputs({input_tensor_x, input_tensor_y}).BindOutputs({output_tensor});
+
+    EXPECT_TRUE(graph->Compile());
+    EXPECT_TRUE(graph->Run());
+    std::vector<float> output(1);
+
+    EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
+    EXPECT_EQ(golden, output);
+}
+
+TEST(Div, shape_5_1_broadcast_uint8) {
+    auto ctx = tim::vx::Context::Create();
+    auto graph = ctx->CreateGraph();
+
+    tim::vx::ShapeType in_shape_x({1});
+    tim::vx::ShapeType in_shape_y({5, 1});
+    tim::vx::ShapeType out_shape({5, 1});
+    tim::vx::Quantization quant(tim::vx::QuantType::ASYMMETRIC, 1, 0);
+    tim::vx::Quantization quant_out(tim::vx::QuantType::ASYMMETRIC, 0.5, 0);
+    tim::vx::TensorSpec input_spec_x(tim::vx::DataType::UINT8,
+                            in_shape_x, tim::vx::TensorAttribute::INPUT, quant);
+    tim::vx::TensorSpec input_spec_y(tim::vx::DataType::UINT8,
+                            in_shape_y, tim::vx::TensorAttribute::INPUT, quant);
+    tim::vx::TensorSpec output_spec(tim::vx::DataType::UINT8,
+                            out_shape, tim::vx::TensorAttribute::OUTPUT, quant_out);
+
+    auto input_tensor_x = graph->CreateTensor(input_spec_x);
+    auto input_tensor_y = graph->CreateTensor(input_spec_y);
+    auto output_tensor = graph->CreateTensor(output_spec);
+
+    std::vector<uint8_t> in_data_x = { 255 };
+    std::vector<uint8_t> in_data_y = { 1, 2, 3, 0, 255 };
+    std::vector<uint8_t> golden = { 255, 255, 170, 255, 2 };
+
+    EXPECT_TRUE(input_tensor_x->CopyDataToTensor(in_data_x.data(), in_data_x.size()));
+    EXPECT_TRUE(input_tensor_y->CopyDataToTensor(in_data_y.data(), in_data_y.size()));
+    auto op = graph->CreateOperation<tim::vx::ops::Div>();
+    (*op).BindInputs({input_tensor_x, input_tensor_y}).BindOutputs({output_tensor});
+
+    EXPECT_TRUE(graph->Compile());
+    EXPECT_TRUE(graph->Run());
+    std::vector<uint8_t> output(5);
+
+    EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
+    EXPECT_EQ(golden, output);
+}
+
+TEST(Div, shape_5_1_broadcast_scale_uint8) {
+    auto ctx = tim::vx::Context::Create();
+    auto graph = ctx->CreateGraph();
+
+    tim::vx::ShapeType in_shape_x({1});
+    tim::vx::ShapeType in_shape_y({5, 1});
+    tim::vx::ShapeType out_shape({5, 1});
+    tim::vx::Quantization quant(tim::vx::QuantType::ASYMMETRIC, 1, 0);
+    tim::vx::Quantization quant_out(tim::vx::QuantType::ASYMMETRIC, 0.5, 0);
+    tim::vx::TensorSpec input_spec_x(tim::vx::DataType::UINT8,
+                            in_shape_x, tim::vx::TensorAttribute::INPUT, quant);
+    tim::vx::TensorSpec input_spec_y(tim::vx::DataType::UINT8,
+                            in_shape_y, tim::vx::TensorAttribute::INPUT, quant);
+    tim::vx::TensorSpec output_spec(tim::vx::DataType::UINT8,
+                            out_shape, tim::vx::TensorAttribute::OUTPUT, quant_out);
+
+    auto input_tensor_x = graph->CreateTensor(input_spec_x);
+    auto input_tensor_y = graph->CreateTensor(input_spec_y);
+    auto output_tensor = graph->CreateTensor(output_spec);
+
+    std::vector<uint8_t> in_data_x = { 128 };
+    std::vector<uint8_t> in_data_y = { 1, 2, 3, 0, 255 };
+    std::vector<uint8_t> golden = { 128, 64, 43, 255, 1 };
+
+    EXPECT_TRUE(input_tensor_x->CopyDataToTensor(in_data_x.data(), in_data_x.size()));
+    EXPECT_TRUE(input_tensor_y->CopyDataToTensor(in_data_y.data(), in_data_y.size()));
+    auto op = graph->CreateOperation<tim::vx::ops::Div>(0.5f);
+    (*op).BindInputs({input_tensor_x, input_tensor_y}).BindOutputs({output_tensor});
+
+    EXPECT_TRUE(graph->Compile());
+    EXPECT_TRUE(graph->Run());
+    std::vector<uint8_t> output(5);
+
+    EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
+    EXPECT_EQ(golden, output);
+}
