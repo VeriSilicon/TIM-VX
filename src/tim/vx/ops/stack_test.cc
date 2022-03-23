947f39cf53471e5c679949faeb9037b3e063fb85
Chen Xin <jack.chen@verisilicon.com>
2022-03-23 13:43:55 +0800

    Added layout inference for stack
    Signed-off-by: Chen Xin <jack.chen@verisilicon.com>

--- src/tim/vx/ops/stack_test.cc
+++ src/tim/vx/ops/stack_test.cc
@@ -23,7 +23,8 @@
 *****************************************************************************/
 #include "tim/vx/context.h"
 #include "tim/vx/graph.h"
-#include "tim/vx/ops/stack.h"
+#include "tim/vx/ops.h"
+#include "tim/transform/layout_inference.h"
 
 #include "gtest/gtest.h"
 
@@ -178,4 +179,74 @@ TEST(Stack, shape_2_3_axis_0) {
     std::vector<float> output(golden.size());
     EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
     EXPECT_EQ(golden, output);
+}
+
+TEST(Stack, LayoutinferernceTest) {
+  auto ctx = tim::vx::Context::Create();
+  auto graph = ctx->CreateGraph();
+
+  tim::vx::ShapeType input_shape({2, 4, 6, 1});
+  tim::vx::ShapeType kernel_shape({2, 2, 2, 3});
+  tim::vx::ShapeType conv2dout_shape({3, 3, 5, 1});
+  tim::vx::ShapeType output_shape({2, 5});
+
+  tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32, input_shape,
+                                 tim::vx::TensorAttribute::INPUT);
+  tim::vx::TensorSpec kernel_spec(tim::vx::DataType::FLOAT32, kernel_shape,
+                                  tim::vx::TensorAttribute::CONSTANT);
+  tim::vx::TensorSpec conv2dout_spec(tim::vx::DataType::FLOAT32,
+                                     conv2dout_shape,
+                                     tim::vx::TensorAttribute::TRANSIENT);
+  tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32, output_shape,
+                                  tim::vx::TensorAttribute::OUTPUT);
+
+  auto input_tensor = graph->CreateTensor(input_spec);
+  auto conv2dout_tensor = graph->CreateTensor(conv2dout_spec);
+  auto output_tensor = graph->CreateTensor(output_spec);
+
+  std::vector<float> in_data = {
+      1, 4, 2, 5, 3, 6, 3, 1, 1, 4, 2, 5, 3, 6, 3, 1, 1, 4, 2, 5, 3, 6, 3, 1,
+      1, 4, 2, 5, 3, 6, 3, 1, 1, 4, 2, 5, 3, 6, 3, 1, 1, 4, 2, 5, 3, 6, 3, 1,
+  };
+  std::vector<float> kernel_data = {
+      1, 4, 2, 5, 1, 2, 3, 6, 1, 4, 2, 5, 1, 2, 3, 6, 1, 4, 2, 5, 1, 2, 3, 6,
+  };
+  std::vector<float> golden = {
+      1, 4, 2, 5, 3, 61, 1, 1, 1, 1,  //fake golden
+  };
+  auto kernel_tensor = graph->CreateTensor(kernel_spec, kernel_data.data());
+
+  // The following parameters have been reverse
+  std::vector<int> begin = {0, 0, 0, 0};
+  std::vector<int> end = {2, 3, 4, 1};
+  std::vector<int> strides = {1, 1, 1, 1};
+  uint32_t MASK_BEGIN = 0, MASK_END = 0b0110, MASK_SHRINK = 0b1010;
+
+  std::array<uint32_t, 2> stride({1, 1});
+  std::array<uint32_t, 2> dilation({1, 1});
+
+  EXPECT_TRUE(input_tensor->CopyDataToTensor(in_data.data(),
+                                             in_data.size() * sizeof(float)));
+
+  auto op1 = graph->CreateOperation<tim::vx::ops::Conv2d>(
+      tim::vx::PadType::VALID, stride, dilation, 0, tim::vx::DataLayout::CWHN);
+  (*op1)
+      .BindInputs({input_tensor, kernel_tensor})
+      .BindOutputs({conv2dout_tensor});
+  auto op2 = graph->CreateOperation<tim::vx::ops::Stack>(
+      begin, end, strides, MASK_BEGIN, MASK_END, MASK_SHRINK);
+  (*op2).BindInputs({conv2dout_tensor}).BindOutputs({output_tensor});
+
+  auto transform = tim::transform::LayoutInference(graph, ctx);
+  auto infer_graph = transform.first;
+  auto graph_io_map = transform.second;
+  auto infer_input = graph_io_map[graph->InputsTensor()[0]];
+  auto infer_output = graph_io_map[graph->OutputsTensor()[0]];
+
+  EXPECT_TRUE(infer_graph->Compile());
+  EXPECT_TRUE(infer_graph->Run());
+
+  std::vector<float> output(golden.size());
+  EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
+  EXPECT_EQ(golden, output);
 }
