19aee44e92d0943fde6ab572bc534ffa3c3dec5a
Chen Xin <jack.chen@verisilicon.com>
2022-03-28 16:13:13 +0800

    Added layout inference & layout test for stack
    Signed-off-by: Chen Xin <jack.chen@verisilicon.com>

---
+++ src/tim/transform/stack_layout_inference_test.cc
@@ -0,0 +1,226 @@
+#include "tim/vx/context.h"
+#include "tim/vx/graph.h"
+#include "tim/vx/ops.h"
+#include "tim/transform/layout_inference.h"
+
+#include "gtest/gtest.h"
+
+TEST(Stack, LayoutinferernceTest_1) {
+  auto ctx = tim::vx::Context::Create();
+  auto graph = ctx->CreateGraph();
+
+  tim::vx::ShapeType input_shape({2, 3, 4, 1});      //cwhn
+  tim::vx::ShapeType kernel_shape({2, 3, 3, 3});     //iwho
+//   tim::vx::ShapeType conv2dout_shape({3, 1, 2, 1});  //cwhn
+  tim::vx::ShapeType output_shape({2, 3, 1, 2, 1});
+
+  tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32, input_shape,
+                                 tim::vx::TensorAttribute::INPUT);
+  tim::vx::TensorSpec kernel_spec(tim::vx::DataType::FLOAT32, kernel_shape,
+                                  tim::vx::TensorAttribute::CONSTANT);
+  tim::vx::TensorSpec conv2dout_spec(tim::vx::DataType::FLOAT32,
+                                     {0,0,0,0},
+                                     tim::vx::TensorAttribute::TRANSIENT);
+  tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32, output_shape,
+                                  tim::vx::TensorAttribute::OUTPUT);
+
+  auto input_tensor = graph->CreateTensor(input_spec);
+  auto conv2dout_tensor = graph->CreateTensor(conv2dout_spec);
+  auto output_tensor = graph->CreateTensor(output_spec);
+
+  std::vector<float> in_data = {
+      1, 1, 1, 1, 2, 0, 5, 3, 6, 3, 1, 1,
+      1, 4, 2, 5, 7, 6, 3, 1, 1, 0, 2, 5,
+  };
+  std::vector<float> kernel_data = {
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 2, 1, 1, 1,
+      0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1,
+      1, 1, 1, 1, 2, 1, 1, 5, 3, 1, 2, 3, 1, 1, 2, 1, 1, 1,
+  };
+  std::vector<float> golden = {
+      64, 77, 49, 44, 81, 97, 64, 77, 49, 44, 81, 97
+  };
+  auto kernel_tensor = graph->CreateTensor(kernel_spec, kernel_data.data());
+
+  std::array<uint32_t, 2> stride({1, 1});
+  std::array<uint32_t, 2> dilation({1, 1});
+
+  auto op1 = graph->CreateOperation<tim::vx::ops::Conv2d>(
+      tim::vx::PadType::VALID, stride, dilation, 0, tim::vx::DataLayout::CWHN);
+  (*op1)
+      .BindInputs({input_tensor, kernel_tensor})
+      .BindOutputs({conv2dout_tensor});
+
+  auto op2 = graph->CreateOperation<tim::vx::ops::Stack>(0, 2);
+  (*op2).BindInputs({conv2dout_tensor, conv2dout_tensor}).BindOutputs({output_tensor});
+
+  auto transform = tim::transform::LayoutInference(graph, ctx);
+  auto infer_graph = transform.first;
+  auto graph_io_map = transform.second;
+  auto infer_input = graph_io_map[graph->InputsTensor()[0]];
+  auto infer_output = graph_io_map[graph->OutputsTensor()[0]];
+  infer_input->CopyDataToTensor(in_data.data(), in_data.size() * sizeof(float));
+
+  EXPECT_TRUE(infer_graph->Compile());
+  EXPECT_TRUE(infer_graph->Run());
+
+  std::vector<float> output(golden.size());
+  EXPECT_TRUE(infer_output->CopyDataFromTensor(output.data()));
+  EXPECT_EQ(golden, output);
+}
+
+TEST(Stack, LayoutinferernceTest_2) {
+  auto ctx = tim::vx::Context::Create();
+  auto graph = ctx->CreateGraph();
+
+  tim::vx::ShapeType input_shape({2, 3, 4, 1});      //cwhn
+  tim::vx::ShapeType kernel_shape({2, 2, 3, 3});     //iwho
+  tim::vx::ShapeType conv2dout_shape({3, 2, 2, 1});  //cwhn
+//   tim::vx::ShapeType output_shape({2, 1, 2, 1});
+  tim::vx::ShapeType output_shape({2, 3, 2});
+
+  tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32, input_shape,
+                                 tim::vx::TensorAttribute::INPUT);
+  tim::vx::TensorSpec kernel_spec(tim::vx::DataType::FLOAT32, kernel_shape,
+                                  tim::vx::TensorAttribute::CONSTANT);
+  tim::vx::TensorSpec conv2dout_spec(tim::vx::DataType::FLOAT32,
+                                     conv2dout_shape,
+                                     tim::vx::TensorAttribute::OUTPUT);
+  tim::vx::TensorSpec reduceout_spec(tim::vx::DataType::FLOAT32,
+                                     {0,0,0},
+                                     tim::vx::TensorAttribute::TRANSIENT);
+  tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32, output_shape,
+                                  tim::vx::TensorAttribute::OUTPUT);
+
+  auto input_tensor = graph->CreateTensor(input_spec);
+  auto conv2dout_tensor = graph->CreateTensor(conv2dout_spec);
+  auto reduceout_tensor = graph->CreateTensor(reduceout_spec);
+  auto output_tensor = graph->CreateTensor(output_spec);
+
+  std::vector<float> in_data = {
+      1, 1, 1, 1, 2, 0, 5, 3, 6, 3, 1, 1,
+      1, 4, 2, 5, 7, 6, 3, 1, 1, 0, 2, 5,
+  };
+  std::vector<float> kernel_data = {
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  };
+  std::vector<float> golden = {
+      33, 33, 37, 35, 35, 43, 34, 34, 58, 39, 39, 43,
+  };
+  auto kernel_tensor = graph->CreateTensor(kernel_spec, kernel_data.data());
+
+  std::array<uint32_t, 2> stride({1, 1});
+  std::array<uint32_t, 2> dilation({1, 1});
+
+  auto op1 = graph->CreateOperation<tim::vx::ops::Conv2d>(
+      tim::vx::PadType::VALID, stride, dilation, 0, tim::vx::DataLayout::CWHN);
+  (*op1)
+      .BindInputs({input_tensor, kernel_tensor})
+      .BindOutputs({conv2dout_tensor});
+  std::vector<int32_t> axis = {2,3};
+  auto op2 = graph->CreateOperation<tim::vx::ops::ReduceMax>(axis, false);
+  (*op2).BindInputs({conv2dout_tensor}).BindOutputs({reduceout_tensor});
+  auto op3 = graph->CreateOperation<tim::vx::ops::Stack>(0, 2);
+  (*op3).BindInputs({reduceout_tensor, reduceout_tensor}).BindOutputs({output_tensor});
+
+  auto transform = tim::transform::LayoutInference(graph, ctx);
+  auto infer_graph = transform.first;
+  auto graph_io_map = transform.second;
+  auto infer_input = graph_io_map[graph->InputsTensor()[0]];
+  auto infer_output = graph_io_map[graph->OutputsTensor()[0]];
+  infer_input->CopyDataToTensor(in_data.data(), in_data.size() * sizeof(float));
+
+  EXPECT_TRUE(infer_graph->Compile());
+  EXPECT_TRUE(infer_graph->Run());
+
+  std::vector<float> output(golden.size());
+  EXPECT_TRUE(infer_output->CopyDataFromTensor(output.data()));
+  EXPECT_EQ(golden, output);
+}
+
+TEST(Stack, LayoutinferernceTest_3) {
+  auto ctx = tim::vx::Context::Create();
+  auto graph = ctx->CreateGraph();
+
+  tim::vx::ShapeType input_shape({2, 3, 4, 1});      //cwhn
+  tim::vx::ShapeType kernel_shape({2, 2, 3, 3});     //iwho
+  tim::vx::ShapeType conv2dout_shape({3, 2, 2, 1});  //cwhn
+  tim::vx::ShapeType output_shape({2, 3, 2, 1});
+
+  tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32, input_shape,
+                                 tim::vx::TensorAttribute::INPUT);
+  tim::vx::TensorSpec kernel_spec(tim::vx::DataType::FLOAT32, kernel_shape,
+                                  tim::vx::TensorAttribute::CONSTANT);
+  tim::vx::TensorSpec conv2dout_spec(tim::vx::DataType::FLOAT32,
+                                     {0,0,0,0},
+                                     tim::vx::TensorAttribute::TRANSIENT);
+  tim::vx::TensorSpec reduceout_spec(tim::vx::DataType::FLOAT32,
+                                     {0,0,0},
+                                     tim::vx::TensorAttribute::TRANSIENT);
+  tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32, output_shape,
+                                  tim::vx::TensorAttribute::OUTPUT);
+
+  auto input_tensor = graph->CreateTensor(input_spec);
+  auto input2_tensor = graph->CreateTensor(input_spec);
+  auto conv2dout_tensor = graph->CreateTensor(conv2dout_spec);
+  auto conv2dout2_tensor = graph->CreateTensor(conv2dout_spec);
+  auto reduceout_tensor = graph->CreateTensor(reduceout_spec);
+  auto reduceout2_tensor = graph->CreateTensor(reduceout_spec);
+  auto output_tensor = graph->CreateTensor(output_spec);
+
+  std::vector<float> in_data = {
+      1, 1, 1, 1, 2, 0, 5, 3, 6, 3, 1, 1,
+      1, 4, 2, 5, 7, 6, 3, 1, 1, 0, 2, 5,
+  };
+  std::vector<float> kernel_data = {
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 2, 1, 1, 1,
+      0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1,
+      1, 1, 1, 1, 2, 1, 1, 5, 3, 1, 2, 3, 1, 1, 2, 1, 1, 1,
+  };
+  std::vector<float> golden = {
+      55, 39, 21, 28, 37, 41, 49, 55, 28, 24, 40, 41
+  };
+  auto kernel_tensor = graph->CreateTensor(kernel_spec, kernel_data.data());
+  auto kernel2_tensor = graph->CreateTensor(kernel_spec, kernel_data.data());
+
+  std::array<uint32_t, 2> stride({1, 1});
+  std::array<uint32_t, 2> dilation({1, 1});
+  auto op1 = graph->CreateOperation<tim::vx::ops::Conv2d>(
+      tim::vx::PadType::VALID, stride, dilation, 0, tim::vx::DataLayout::CWHN);
+  (*op1)
+      .BindInputs({input_tensor, kernel_tensor})
+      .BindOutputs({conv2dout_tensor});
+  auto op11 = graph->CreateOperation<tim::vx::ops::Conv2d>(
+      tim::vx::PadType::VALID, stride, dilation, 0, tim::vx::DataLayout::CWHN);
+  (*op11)
+      .BindInputs({input2_tensor, kernel2_tensor})
+      .BindOutputs({conv2dout2_tensor});
+
+  std::vector<int32_t> axis = {1};
+  auto op2 = graph->CreateOperation<tim::vx::ops::ReduceMax>(axis, false);
+  (*op2).BindInputs({conv2dout_tensor}).BindOutputs({reduceout_tensor});
+  axis = {2};
+  auto op22 = graph->CreateOperation<tim::vx::ops::ReduceMax>(axis, false);
+  (*op22).BindInputs({conv2dout2_tensor}).BindOutputs({reduceout2_tensor});
+
+  auto op3 = graph->CreateOperation<tim::vx::ops::Stack>(0, 2);
+  (*op3).BindInputs({reduceout_tensor, reduceout2_tensor}).BindOutputs({output_tensor});
+
+  auto transform = tim::transform::LayoutInference(graph, ctx);
+  auto infer_graph = transform.first;
+  auto graph_io_map = transform.second;
+  auto infer_input = graph_io_map[graph->InputsTensor()[0]];
+  auto infer_input2 = graph_io_map[graph->InputsTensor()[1]];
+  auto infer_output = graph_io_map[graph->OutputsTensor()[0]];
+  infer_input->CopyDataToTensor(in_data.data(), in_data.size() * sizeof(float));
+  infer_input2->CopyDataToTensor(in_data.data(), in_data.size() * sizeof(float));
+
+  EXPECT_TRUE(infer_graph->Compile());
+  EXPECT_TRUE(infer_graph->Run());
+
+  std::vector<float> output(golden.size());
+  EXPECT_TRUE(infer_output->CopyDataFromTensor(output.data()));
+  EXPECT_EQ(golden, output);
+}
--- src/tim/vx/ops/stack_test.cc
+++ src/tim/vx/ops/stack_test.cc
@@ -23,228 +23,146 @@
 *****************************************************************************/
 #include "tim/vx/context.h"
 #include "tim/vx/graph.h"
-#include "tim/vx/ops.h"
-#include "tim/transform/layout_inference.h"
+#include "tim/vx/ops/stack.h"
 
 #include "gtest/gtest.h"
 
-TEST(Stack, shape_2_3_axis_2) {
-    auto ctx = tim::vx::Context::Create();
-    auto graph = ctx->CreateGraph();
-
-    tim::vx::ShapeType input_shape({2,3});
-    tim::vx::ShapeType output_shape({2,3,2});
-    tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32,
-        input_shape, tim::vx::TensorAttribute::INPUT);
-    tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32,
-        output_shape, tim::vx::TensorAttribute::OUTPUT);
-
-    auto input_tensor1 = graph->CreateTensor(input_spec);
-    auto input_tensor2 = graph->CreateTensor(input_spec);
-    auto output_tensor = graph->CreateTensor(output_spec);
-
-    std::vector<float> in_data1 = {
-        1,4,
-        2,5,
-        3,6
-    };
-    std::vector<float> in_data2 = {
-        1,4,
-        2,5,
-        3,6
-    };
-    std::vector<float> golden = {
-        1,4,
-        2,5,
-        3,6,
-
-        1,4,
-        2,5,
-        3,6
-        };
-
-    EXPECT_TRUE(input_tensor1->CopyDataToTensor(
-        in_data1.data(), in_data1.size() * sizeof(float)));
-    EXPECT_TRUE(input_tensor2->CopyDataToTensor(
-        in_data2.data(), in_data2.size() * sizeof(float)));
-    auto op = graph->CreateOperation<tim::vx::ops::Stack>(2, 2);
-    (*op).BindInputs({input_tensor1,input_tensor2}).BindOutputs(
-        {output_tensor});
-
-    EXPECT_TRUE(graph->Compile());
-    EXPECT_TRUE(graph->Run());
-
-    std::vector<float> output(golden.size());
-    EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
-    EXPECT_EQ(golden, output);
-}
-
-TEST(Stack, shape_2_3_axis_1) {
-    auto ctx = tim::vx::Context::Create();
-    auto graph = ctx->CreateGraph();
-
-    tim::vx::ShapeType input_shape({2,3});
-    tim::vx::ShapeType output_shape({2,3,2});
-    tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32,
-        input_shape, tim::vx::TensorAttribute::INPUT);
-    tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32,
-        output_shape, tim::vx::TensorAttribute::OUTPUT);
-
-    auto input_tensor1 = graph->CreateTensor(input_spec);
-    auto input_tensor2 = graph->CreateTensor(input_spec);
-    auto output_tensor = graph->CreateTensor(output_spec);
-
-    std::vector<float> in_data1 = {
-        1,4,
-        2,5,
-        3,6
-    };
-    std::vector<float> in_data2 = {
-        1,4,
-        2,5,
-        3,6
-    };
-    std::vector<float> golden = {
-        1,4,
-        1,4,
-        2,5,
-
-        2,5,
-        3,6,
-        3,6,
-        };
-
-    EXPECT_TRUE(input_tensor1->CopyDataToTensor(
-        in_data1.data(), in_data1.size() * sizeof(float)));
-    EXPECT_TRUE(input_tensor2->CopyDataToTensor(
-        in_data2.data(), in_data2.size() * sizeof(float)));
-    auto op = graph->CreateOperation<tim::vx::ops::Stack>(1, 2);
-    (*op).BindInputs({input_tensor1,input_tensor2}).BindOutputs(
-        {output_tensor});
-
-    EXPECT_TRUE(graph->Compile());
-    EXPECT_TRUE(graph->Run());
-
-    std::vector<float> output(golden.size());
-    EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
-    EXPECT_EQ(golden, output);
-}
-
-TEST(Stack, shape_2_3_axis_0) {
-    auto ctx = tim::vx::Context::Create();
-    auto graph = ctx->CreateGraph();
-
-    tim::vx::ShapeType input_shape({2,3});
-    tim::vx::ShapeType output_shape({2,3,2});
-    tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32,
-        input_shape, tim::vx::TensorAttribute::INPUT);
-    tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32,
-        output_shape, tim::vx::TensorAttribute::OUTPUT);
-
-    auto input_tensor1 = graph->CreateTensor(input_spec);
-    auto input_tensor2 = graph->CreateTensor(input_spec);
-    auto output_tensor = graph->CreateTensor(output_spec);
-
-    std::vector<float> in_data1 = {
-        1,4,
-        2,5,
-        3,6
-    };
-    std::vector<float> in_data2 = {
-        1,4,
-        2,5,
-        3,6
-    };
-    std::vector<float> golden = {
-        1, 1,
-        4, 4,
-        2, 2,
-
-        5, 5,
-        3, 3,
-        6, 6
-        };
-
-    EXPECT_TRUE(input_tensor1->CopyDataToTensor(
-        in_data1.data(), in_data1.size() * sizeof(float)));
-    EXPECT_TRUE(input_tensor2->CopyDataToTensor(
-        in_data2.data(), in_data2.size() * sizeof(float)));
-    auto op = graph->CreateOperation<tim::vx::ops::Stack>(0, 2);
-    (*op).BindInputs({input_tensor1,input_tensor2}).BindOutputs(
-        {output_tensor});
-
-    EXPECT_TRUE(graph->Compile());
-    EXPECT_TRUE(graph->Run());
-
-    std::vector<float> output(golden.size());
-    EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
-    EXPECT_EQ(golden, output);
-}
-
-TEST(Stack, LayoutinferernceTest) {
+TEST(Stack, shape_3_4_axis_2) {
   auto ctx = tim::vx::Context::Create();
   auto graph = ctx->CreateGraph();
 
-  tim::vx::ShapeType input_shape({2, 4, 6, 1});
-  tim::vx::ShapeType kernel_shape({2, 2, 2, 3});
-  tim::vx::ShapeType conv2dout_shape({3, 3, 5, 1});
-  tim::vx::ShapeType output_shape({2, 5});
-
+  tim::vx::ShapeType input_shape({4, 3});
+  tim::vx::ShapeType output_shape({4, 3, 2});
   tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32, input_shape,
                                  tim::vx::TensorAttribute::INPUT);
-  tim::vx::TensorSpec kernel_spec(tim::vx::DataType::FLOAT32, kernel_shape,
-                                  tim::vx::TensorAttribute::CONSTANT);
-  tim::vx::TensorSpec conv2dout_spec(tim::vx::DataType::FLOAT32,
-                                     conv2dout_shape,
-                                     tim::vx::TensorAttribute::TRANSIENT);
   tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32, output_shape,
                                   tim::vx::TensorAttribute::OUTPUT);
 
-  auto input_tensor = graph->CreateTensor(input_spec);
-  auto conv2dout_tensor = graph->CreateTensor(conv2dout_spec);
+  auto input_tensor1 = graph->CreateTensor(input_spec);
+  auto input_tensor2 = graph->CreateTensor(input_spec);
   auto output_tensor = graph->CreateTensor(output_spec);
 
   std::vector<float> in_data = {
-      1, 4, 2, 5, 3, 6, 3, 1, 1, 4, 2, 5, 3, 6, 3, 1, 1, 4, 2, 5, 3, 6, 3, 1,
-      1, 4, 2, 5, 3, 6, 3, 1, 1, 4, 2, 5, 3, 6, 3, 1, 1, 4, 2, 5, 3, 6, 3, 1,
-  };
-  std::vector<float> kernel_data = {
-      1, 4, 2, 5, 1, 2, 3, 6, 1, 4, 2, 5, 1, 2, 3, 6, 1, 4, 2, 5, 1, 2, 3, 6,
+      2, 1, 0, 1,
+      2, 4, 4, 4,
+      3, 2, 1, 4,
   };
   std::vector<float> golden = {
-      1, 4, 2, 5, 3, 61, 1, 1, 1, 1,  //fake golden
+      2, 1, 0, 1,
+      2, 4, 4, 4,
+      3, 2, 1, 4,
+      2, 1, 0, 1,
+      2, 4, 4, 4,
+      3, 2, 1, 4,
   };
-  auto kernel_tensor = graph->CreateTensor(kernel_spec, kernel_data.data());
 
-  // The following parameters have been reverse
-  std::vector<int> begin = {0, 0, 0, 0};
-  std::vector<int> end = {2, 3, 4, 1};
-  std::vector<int> strides = {1, 1, 1, 1};
-  uint32_t MASK_BEGIN = 0, MASK_END = 0b0110, MASK_SHRINK = 0b1010;
+  EXPECT_TRUE(input_tensor1->CopyDataToTensor(in_data.data(),
+                                              in_data.size() * sizeof(float)));
+  EXPECT_TRUE(input_tensor2->CopyDataToTensor(in_data.data(),
+                                              in_data.size() * sizeof(float)));
+  auto op = graph->CreateOperation<tim::vx::ops::Stack>(2, 2);
+  (*op).BindInputs({input_tensor1, input_tensor2}).BindOutputs({output_tensor});
 
-  std::array<uint32_t, 2> stride({1, 1});
-  std::array<uint32_t, 2> dilation({1, 1});
+  EXPECT_TRUE(graph->Compile());
+  EXPECT_TRUE(graph->Run());
 
-  EXPECT_TRUE(input_tensor->CopyDataToTensor(in_data.data(),
-                                             in_data.size() * sizeof(float)));
+  std::vector<float> output(golden.size());
+  EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
+  EXPECT_EQ(golden, output);
+}
 
-  auto op1 = graph->CreateOperation<tim::vx::ops::Conv2d>(
-      tim::vx::PadType::VALID, stride, dilation, 0, tim::vx::DataLayout::CWHN);
-  (*op1)
-      .BindInputs({input_tensor, kernel_tensor})
-      .BindOutputs({conv2dout_tensor});
-  auto op2 = graph->CreateOperation<tim::vx::ops::Stack>(
-      begin, end, strides, MASK_BEGIN, MASK_END, MASK_SHRINK);
-  (*op2).BindInputs({conv2dout_tensor}).BindOutputs({output_tensor});
+TEST(Stack, shape_3_4_axis_1) {
+  auto ctx = tim::vx::Context::Create();
+  auto graph = ctx->CreateGraph();
 
-  auto transform = tim::transform::LayoutInference(graph, ctx);
-  auto infer_graph = transform.first;
-  auto graph_io_map = transform.second;
-  auto infer_input = graph_io_map[graph->InputsTensor()[0]];
-  auto infer_output = graph_io_map[graph->OutputsTensor()[0]];
+  tim::vx::ShapeType input_shape({4, 3});
+  tim::vx::ShapeType output_shape({4, 2, 3});
+  tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32, input_shape,
+                                 tim::vx::TensorAttribute::INPUT);
+  tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32, output_shape,
+                                  tim::vx::TensorAttribute::OUTPUT);
 
-  EXPECT_TRUE(infer_graph->Compile());
-  EXPECT_TRUE(infer_graph->Run());
+  auto input_tensor1 = graph->CreateTensor(input_spec);
+  auto input_tensor2 = graph->CreateTensor(input_spec);
+  auto output_tensor = graph->CreateTensor(output_spec);
+
+  std::vector<float> in_data = {
+      2, 1, 0, 1,
+      2, 4, 4, 4,
+      3, 2, 1, 4,
+  };
+  std::vector<float> golden = {
+        2, 1, 0, 1,
+        2, 1, 0, 1,
+
+        2, 4, 4, 4,
+        2, 4, 4, 4,
+
+        3, 2, 1, 4,
+        3, 2, 1, 4,
+  };
+
+  EXPECT_TRUE(input_tensor1->CopyDataToTensor(in_data.data(),
+                                              in_data.size() * sizeof(float)));
+  EXPECT_TRUE(input_tensor2->CopyDataToTensor(in_data.data(),
+                                              in_data.size() * sizeof(float)));
+  auto op = graph->CreateOperation<tim::vx::ops::Stack>(1, 2);
+  (*op).BindInputs({input_tensor1, input_tensor2}).BindOutputs({output_tensor});
+
+  EXPECT_TRUE(graph->Compile());
+  EXPECT_TRUE(graph->Run());
+
+  std::vector<float> output(golden.size());
+  EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
+  EXPECT_EQ(golden, output);
+}
+
+TEST(Stack, shape_3_4_axis_0) {
+  auto ctx = tim::vx::Context::Create();
+  auto graph = ctx->CreateGraph();
+
+  tim::vx::ShapeType input_shape({4, 3});
+  tim::vx::ShapeType output_shape({2, 4, 3});
+  tim::vx::TensorSpec input_spec(tim::vx::DataType::FLOAT32, input_shape,
+                                 tim::vx::TensorAttribute::INPUT);
+  tim::vx::TensorSpec output_spec(tim::vx::DataType::FLOAT32, output_shape,
+                                  tim::vx::TensorAttribute::OUTPUT);
+
+  auto input_tensor1 = graph->CreateTensor(input_spec);
+  auto input_tensor2 = graph->CreateTensor(input_spec);
+  auto output_tensor = graph->CreateTensor(output_spec);
+
+  std::vector<float> in_data = {
+      2, 1, 0, 1,
+      2, 4, 4, 4,
+      3, 2, 1, 4,
+  };
+  std::vector<float> golden = {
+        2, 2,
+        1, 1,
+        0, 0,
+        1, 1,
+
+        2, 2,
+        4, 4,
+        4, 4,
+        4, 4,
+
+        3, 3,
+        2, 2,
+        1, 1,
+        4, 4,
+  };
+
+  EXPECT_TRUE(input_tensor1->CopyDataToTensor(in_data.data(),
+                                              in_data.size() * sizeof(float)));
+  EXPECT_TRUE(input_tensor2->CopyDataToTensor(in_data.data(),
+                                              in_data.size() * sizeof(float)));
+  auto op = graph->CreateOperation<tim::vx::ops::Stack>(0, 2);
+  (*op).BindInputs({input_tensor1, input_tensor2}).BindOutputs({output_tensor});
+
+  EXPECT_TRUE(graph->Compile());
+  EXPECT_TRUE(graph->Run());
 
   std::vector<float> output(golden.size());
   EXPECT_TRUE(output_tensor->CopyDataFromTensor(output.data()));
