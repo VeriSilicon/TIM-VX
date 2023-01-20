0cc445ae528a918bc574d15f4256e92a7170c29f
Tang <jing.tang@verisilicon.com>
2023-01-20 11:38:21 +0800

    update copyright information

--- src/tim/vx/ops/reduce.cc
+++ src/tim/vx/ops/reduce.cc
@@ -1,6 +1,6 @@
 /****************************************************************************
 *
-*    Copyright (c) 2020 Vivante Corporation
+*    Copyright (c) 2020-2023 Vivante Corporation
 *
 *    Permission is hereby granted, free of charge, to any person obtaining a
 *    copy of this software and associated documentation files (the "Software"),
