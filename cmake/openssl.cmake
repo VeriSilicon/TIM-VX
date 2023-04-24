9a366d31b0f127eb85c7489e31eb23f4d736b54d
Chen Xin <jack.chen@verisilicon.com>
2023-04-24 17:23:19 +0800

    fixed openssl build issue on android
    Type: bug fix
    
    Signed-off-by: Chen Xin <jack.chen@verisilicon.com>

---
+++ cmake/openssl.cmake
@@ -0,0 +1,14 @@
+include(FetchContent)
+message("Openssl version 3.0")
+FetchContent_Declare(
+    openssl
+    GIT_REPOSITORY git@gitlab-cn.verisilicon.com:npu_sw/openssl/openssl-cmake.git
+    GIT_TAG 79c122d1606556610477cfae07ff27d8c6e5f260)
+
+set(openssl_force_shared_crt ON CACHE BOOL "" FORCE)
+set(INSTALL_openssl OFF CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(openssl)
+message("Apply dynamic_openssl patch on openssl-cmake")
+execute_process(COMMAND git apply ${CMAKE_SOURCE_DIR}/cmake/dynamic_openssl.patch
+                WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/_deps/openssl-src)
