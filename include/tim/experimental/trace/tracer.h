6f34b66ae40881b5977c49f83910bed8dac8925d
MercuryChen <52600596+MercuryChen@users.noreply.github.com>
2023-08-25 00:41:45 +0800

    Split replayer code from tracer.h (#642)
    * Add support for different input dtype of MaxPoolGrad.
    
    Type: Code improvement
    
    * Integrate api trace into tim-vx source code, as part of experimeantal.
    
    Type: New Feature
    
    * Refine api trace code and document
    Add missing traced apis of tim::vx::Quantization
    
    Type: Code improvement
    
    * Split Api relayer code out of tracer.
    To enable compile replayer code in machine which can't access high version boost libs.
    
    Type: Code improvement

--- include/tim/experimental/trace/tracer.h
+++ include/tim/experimental/trace/tracer.h
@@ -54,11 +54,10 @@
 #include <boost/hana/transform.hpp>
 #include <boost/hana/unpack.hpp>
 
+#include "tim/experimental/trace/replayer.h"
 
 #define TRACE_LOG_NAME_ "trace_log.cc"
 #define TRACE_BIN_FILE_ "trace_bin.bin"
-#define TRACE_LOG_FILE_REPLAY_ "trace_log.rpl.cc"
-#define TRACE_BIN_FILE_REPLAY_ "trace_bin.rpl.bin"
 #define TRACE_PREFIX_ENV_VAR_ "TRACE_DUMP_PREFIX" 
 
 #define TCLOGE(fmt, ...) do {                                                  \
@@ -137,94 +136,6 @@ struct is_not_traced_obj_like : std::integral_constant<bool,
 
 } /* namespace trace */
 
-/*************************** definition of replayer ***************************/
-namespace trace {
-
-class Replayer {
-  static FILE* file_trace_bin_;
-  static FILE* open_file(const char* file_name);
- public:
-  template <class T>
-  static std::vector<T> get_vector(uint32_t offset, size_t vec_size);
-
-  template <class T>
-  static std::vector<T> get_vector_from(
-      const char* file_path, uint32_t offset, size_t vec_size);
-
-  template <class T, std::size_t N>
-  static std::array<T, N> get_array(uint32_t offset, size_t vec_size);
-};
-
-// #define API_REPLAYER_IMPLEMENTATION
-#ifdef API_REPLAYER_IMPLEMENTATION
-FILE* Replayer::file_trace_bin_ = Replayer::open_file(TRACE_BIN_FILE_REPLAY_);
-/* static */ FILE* Replayer::open_file(const char* file_name) {
-  char* prefix = getenv(TRACE_PREFIX_ENV_VAR_);
-  FILE* fp;
-  char path[1024] = {0};
-  if (prefix != NULL) {
-    strcpy(path, prefix);
-    strcat(path, file_name);
-  } else {
-    strcpy(path, file_name);
-  }
-  fp = fopen(path, "r");
-  if (!fp) {
-    TCLOGE("Can not open file at: %s\n", path);
-  }
-  return fp;
-}
-
-template <class T>
-/* static */ std::vector<T> Replayer::get_vector(
-    uint32_t offset, size_t vec_size) {
-  std::vector<T> ret_vec;
-  if (!file_trace_bin_) {
-    TCLOGE("FILE pointer is NULL!\n");
-  } else {
-    T* buffer = new T[vec_size];
-    fseek(file_trace_bin_, offset, SEEK_SET);
-    if (fread(buffer, sizeof(T), vec_size, file_trace_bin_) == vec_size) {
-      ret_vec.assign(buffer, buffer + vec_size);
-    } else {
-      TCLOGE("Read binary data failed!\n");
-    }
-    delete[] buffer;
-  }
-  return ret_vec;
-}
-
-template <class T, std::size_t N>
-/* static */ std::array<T, N> Replayer::get_array(
-    uint32_t offset, size_t vec_size) {
-  std::vector<T> ret_vec = get_vector<T>(offset, vec_size);
-  std::array<T, N> ret_arr;
-  std::copy_n(ret_vec.begin(), ret_vec.size(), ret_arr.begin());
-  return ret_arr;
-}
-
-template <class T>
-/* static */ std::vector<T> Replayer::get_vector_from(
-    const char* file_path,uint32_t offset, size_t vec_size) {
-  FILE* external_file = fopen(file_path, "r");
-  if (!external_file) {
-    TCLOGE("Can not open file at: %s\n", file_path);
-  }
-  std::vector<T> ret_vec;
-  T* buffer = new T[vec_size];
-  fseek(external_file, offset, SEEK_SET);
-  if (fread(buffer, sizeof(T), vec_size, external_file) == vec_size) {
-    ret_vec.assign(buffer, buffer + vec_size);
-  } else {
-    TCLOGE("Read binary data failed!\n");
-  }
-  delete[] buffer;
-  return ret_vec;
-}
-
-#endif /* #ifdef API_TRACER_IMPLEMENTATION */
-
-} /* namespace trace */
 
 /**************************** definition of tracer ****************************/
 namespace trace {
