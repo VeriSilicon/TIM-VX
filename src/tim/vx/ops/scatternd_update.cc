c23f6fa1ad71908fb239348f990e6b18be19f2fb
unknown <z0026@china.oritek.com.cn>
2023-09-26 16:51:16 +0800

    Add ScatterND_Update operator

---
+++ src/tim/vx/ops/scatternd_update.cc
@@ -0,0 +1,45 @@
+/****************************************************************************
+*
+*    Copyright (c) 2020-2023 Vivante Corporation
+*
+*    Permission is hereby granted, free of charge, to any person obtaining a
+*    copy of this software and associated documentation files (the "Software"),
+*    to deal in the Software without restriction, including without limitation
+*    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+*    and/or sell copies of the Software, and to permit persons to whom the
+*    Software is furnished to do so, subject to the following conditions:
+*
+*    The above copyright notice and this permission notice shall be included in
+*    all copies or substantial portions of the Software.
+*
+*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+*    DEALINGS IN THE SOFTWARE.
+*
+*****************************************************************************/
+#include "tim/vx/ops/scatternd_update.h"
+
+#include "builtin_op_impl.h"
+#include "vsi_nn_pub.h"
+
+namespace tim {
+namespace vx {
+namespace ops {
+
+ScatterND_Update::ScatterND_Update(Graph* graph, const std::vector<uint32_t>& shape)
+    : BuiltinOp(graph, VSI_NN_OP_SCATTER_ND_UPDATE), shape_(shape) {
+  this->impl()->node()->nn_param.scatter_nd.dim_num = shape_.size();
+  this->impl()->node()->nn_param.scatter_nd.shape = shape_.data();
+}
+
+std::shared_ptr<Operation> ScatterND_Update::Clone(std::shared_ptr<Graph>& graph) const {
+  return graph->CreateOperation<ScatterND_Update>(this->shape_);
+}
+
+}  // namespace ops
+}  // namespace vx
+}  // namespace tim
